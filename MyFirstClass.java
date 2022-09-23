f12f067adc205124413ac68208d8e4a0da6faa23
luca <52205286@edu.campus02.at>
2022-09-23 17:01:51 +0200

    added MyFirstClass

---
+++ MyFirstClass.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+//meine erste klasse...wow
+public class MyFirstClass {
+
+
+    //meine erste main methode...cool
+    //shortcut fuer main method -> psvm/main
+    public static void main(String[] args) {
+
+        //erste ausgabe
+        System.out.println("Hi Luca wie gehts?");
+
+        //shortcut fuer system out println -> sout
+        System.out.println(Arrays.toString(args));
+
+
+    }
+}
